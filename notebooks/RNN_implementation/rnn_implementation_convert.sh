a6f6e92b8d021c66cbed4c16896030c50d036e01
Peter Roelants <peter.roelants+git@gmail.com>
2015-10-04 18:01:24 +0100

    reset git history

---
+++ notebooks/RNN_implementation/rnn_implementation_convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash bash
+
+template="notebook_simple_rnn_post"
+
+sh ../nb_converter.sh rnn_implementation_part01.ipynb "How to implement a recurrent neural network Part 1" "2015-09-27" $template '1 5 6 7 10'
+
+sh ../nb_converter.sh rnn_implementation_part02.ipynb "How to implement a recurrent neural network Part 2" "2015-09-27" $template '1 2 3 6 10 12 13'
