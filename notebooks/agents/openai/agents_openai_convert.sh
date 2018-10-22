a025c65c253ce96cec11ef340f62efa52d278fd3
Peter Roelants <Peter.roelants@gmail.com>
2018-10-22 19:29:18 +0100

    Blogposts clean commit

---
+++ notebooks/agents/openai/agents_openai_convert.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+layout="post"
+subdir="blog/agents/openai/"
+
+./notebooks/notebook_convert.py \
+    --nbpath notebooks/agents/openai/react-openai-function-calling.ipynb \
+    --date "2014-01-21" \
+    --layout $layout \
+    --subdir ${subdir} \
+    --description "Simple example of using OpenAI Function Calling in a ReAct Loop to execute simple multistep tasks." \
+    --tags "LLM" "ReAct Loop" "OpenAI" "Agents"
